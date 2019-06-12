e2dfe866b97e92856d448967482b134de4936226
Dante Moore <dante@Dantes-MacBook-Pro.local>
2019-06-12 15:42:23 -0700

    axios completed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    // this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div>
@@ -11,7 +21,7 @@ class App extends Component {
           <Navbar title="Github Finder" icon="fab fa-github" />
         </nav>
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,44 +2,17 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "DanteCypress",
-        avatar_url: "https://avatars1.githubusercontent.com/u/37960812?v=4",
-        html_url: "https://github.com/DanteCypress"
-      },
-      {
-        id: "2",
-        login: "a-visai",
-        avatar_url: "https://avatars1.githubusercontent.com/u/47129670?v=4",
-        html_url: "https://github.com/a-visai"
-      },
-      {
-        id: "3",
-        login: "MuzitK",
-        avatar_url: "https://avatars1.githubusercontent.com/u/47538843?v=4",
-        html_url: "https://github.com/muzitk"
-      },
-      {
-        id: "4",
-        login: "Elshadai1",
-        avatar_url: "https://avatars1.githubusercontent.com/u/47315015?v=4",
-        html_url: "https://github.com/Elshadai1"
-      }
-    ]
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
 }
+
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3,1fr)",
