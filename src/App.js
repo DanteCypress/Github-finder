6c7dcf3f2020299c40c25aeb1dc90d87425103a2
Dante Moore <dante@Dantes-MacBook-Pro.local>
2019-06-12 14:30:54 -0700

    useritem added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layouts/Navbar";
+import Users from "./components/users/Users";
 
 class App extends Component {
   render() {
     return (
-      <nav className="navbar bg-primary">
-        <Navbar />
-      </nav>
+      <div>
+        <nav className="navbar bg-primary">
+          <Navbar title="Github Finder" icon="fab fa-github" />
+        </nav>
+        <Users />
+      </div>
     );
   }
 }
