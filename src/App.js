ba08e393042413140a997444e10563e5040c4f81
Dante Moore <dante@Dantes-MBP.attlocal.net>
2019-06-12 23:22:25 -0700

    searchbar and alert completed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Navbar from "./components/layouts/Navbar";
 import Users from "./components/users/Users";
 import axios from "axios";
 import Search from "./components/users/Search";
+import Alert from "./components/layouts/Alert";
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
   async componentDidMount() {
     console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
@@ -19,6 +21,20 @@ class App extends Component {
     }`);
     this.setState({ users: res.data, loading: false });
   }
+  //Search Github users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${
+      process.env.REACT_APP_GITHUB_CLIENT_SECRET
+    }`);
+    this.setState({ users: res.data.items, loading: false });
+  };
+  //Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+    setTimeout(() => this.setState({ alert: null }), 4000);
+  };
   render() {
     return (
       <div>
@@ -27,7 +43,8 @@ class App extends Component {
         </nav>
 
         <div className="container">
-          <Search />
+          <Alert alert={this.state.alert} />
+          <Search searchUsers={this.searchUsers} setAlert={this.setAlert} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
