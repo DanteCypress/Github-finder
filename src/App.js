f362c41265da9f68a7bd61e661f2c4b387081592
Dante Moore <dante@Dantes-MBP.attlocal.net>
2019-06-14 00:24:36 -0700

    user added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 import Users from "./components/users/Users";
 import axios from "axios";
 import Search from "./components/users/Search";
 import Alert from "./components/layouts/Alert";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./components/pages/About";
+import User from "./components/users/User";
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
-    alert: null
+    alert: null,
+    user: {}
   };
   async componentDidMount() {
     console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
@@ -30,24 +34,69 @@ class App extends Component {
     }`);
     this.setState({ users: res.data.items, loading: false });
   };
+
+  //Single user search
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${
+        process.env.REACT_APP_GITHUB_CLIENT_SECRET
+      }`
+    );
+    this.setState({ user: res.data, loading: false });
+  };
   //Set Alert
   setAlert = (msg, type) => {
     this.setState({ alert: { msg: msg, type: type } });
     setTimeout(() => this.setState({ alert: null }), 4000);
   };
   render() {
+    const { user, loading } = this.state;
     return (
-      <div>
-        <nav className="navbar bg-primary">
-          <Navbar title="Github Finder" icon="fab fa-github" />
-        </nav>
+      <Router>
+        <div>
+          <nav className="navbar bg-primary">
+            <Navbar title="Github Finder" icon="fab fa-github" />
+          </nav>
 
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search searchUsers={this.searchUsers} setAlert={this.setAlert} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      loading={this.state.loading}
+                      users={this.state.users}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
